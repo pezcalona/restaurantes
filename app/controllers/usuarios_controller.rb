fad2a493387a1d7aedfb841de09ab28d6abeb1f6
Pablo Escalona Osorio <pa.escalona.o@gmail.com>
2021-10-22 21:09:51 -0300

    Mostrar Usuario

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -4,13 +4,17 @@ class UsuariosController < ApplicationController
     def crear
         @usuario = Usuario.new
     end
+    #get/usuario/id
+    def mostrar
+        @usuario = Usuario.find(params[:id])
+    end
 
     #post/usuarios
     def guardar
         datos_usuario = params.require(:usuario).permit(:nombre_usuario, :password, :password_confirmation)
         @usuario = Usuario.new(datos_usuario)
         if @usuario.save
-            #mostrar un mensaje o vista de confirmación
+            redirect_to usuario_path(@usuario)
         else
             render :crear
         end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 #usuario
-  get   'usuarios/nuevo',     to: 'usuarios#crear',     as:'nuevo_usuario' 
+  get   'usuarios/nuevo',     to: 'usuarios#crear',     as:'nuevo_usuario'
+  get   'usuarios/:id',       to: 'usuarios#mostrar',    as:'usuario'
+  
   post  'usuarios',           to: 'usuarios#guardar',   as: 'usuarios'
 end
